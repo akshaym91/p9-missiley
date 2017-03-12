9caa56d1fc9f4a65ac742a431f7f4005dae92414
akshaym91 <akshaym_91@yahoo.co.in>
2017-03-12 16:47:11 +0530

    Changed path

--- app.py
+++ app.py
@@ -113,7 +113,7 @@ def gconnect():
 
     try:
         # Constuction of a credentials object
-        oauth_flow = flow_from_clientsecrets('client_secrets.json', scope='')
+        oauth_flow = flow_from_clientsecrets('/var/www/html/client_secrets.json', scope='')
         oauth_flow.redirect_uri = 'postmessage'
         credentials = oauth_flow.step2_exchange(code)
     except FlowExchangeError:
@@ -232,10 +232,10 @@ def fbconnect():
     access_token = request.data
 
     # Read the client secret from the file
-    app_id = json.loads(open('fb_client_secrets.json', 'r').read())[
+    app_id = json.loads(open('/var/www/html/fb_client_secrets.json', 'r').read())[
         'web']['app_id']
     app_secret = json.loads(
-        open('fb_client_secrets.json', 'r').read())['web']['app_secret']
+        open('/var/www/html/fb_client_secrets.json', 'r').read())['web']['app_secret']
     url = 'https://graph.facebook.com/oauth/access_token?grant_type=\
     fb_exchange_token&client_id=%s&client_secret=%s&fb_exchange_token=%s' % (
         app_id, app_secret, access_token)
